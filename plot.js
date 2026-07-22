14d392238031ebe1268a74737f1e46d344a06dcc
RNGDelak <phamlehaibang331@gmail.com>
2026-07-22 22:24:55 +0700

    Update plot.js

--- plot.js
+++ plot.js
@@ -310,7 +310,7 @@ function drawTimelineLabels() {
 function drawHUD() {
     let py = 40;
     const px = canvas.width - 7;
-    createTextLabel(notation.title, "rgb(255,255,255)", px, 7, "right", "top", "30px Bold Serif");
+    createTextLabel(notation.title, "rgb(255,255,255)", px, 7, "right", "top", "bold 30px Serif");
         notation.ordinalTypes.forEach(([name, color]) => {
         createTextLabel(name, color, px, py, "right", "top", "26px Serif");
         py += 30;
